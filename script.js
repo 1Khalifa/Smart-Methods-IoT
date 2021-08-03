95ccc5df829ce007885c1f71a2c4fccf2e5f4e7e
1khalifa <xikhalifa@outlook.com>
2021-08-03 11:04:31 +0300

    watson update

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let lang = "English"
+let en = null;
+if (lang === "English") {
+
+    en = window.watsonAssistantChatOptions = {
+        integrationID: "46ea18cb-13f7-45bf-b5f4-61dfe548579c", // The ID of this integration.
+        region: "eu-gb", // The region your integration is hosted in.
+        serviceInstanceID: "47e2608d-3af4-4977-af92-c37729e13315", // The ID of your service instance.
+        onLoad: function (instance) {
+            instance.render();
+        }
+    };
+
+    setTimeout(function () {
+        const t = document.createElement('script');
+        t.setAttribute("id", "eng");
+        t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+        document.head.appendChild(t);
+    });
+}
+
 var eng1 = document.getElementById("eng1");
 var eng2 = document.getElementById("eng2");
 var eng3 = document.getElementById("eng3");
@@ -37,3 +58,28 @@ eng3.addEventListener("input", rangeValue);
 eng4.addEventListener("input", rangeValue);
 eng5.addEventListener("input", rangeValue);
 eng6.addEventListener("input", rangeValue);
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            lang = "Arabic"
+            movementTab.innerHTML = " اتجاه حركة الروبوت  <img class=\"directions ms-1\" src=\"images/controller.svg\">";
+            movementTab.classList.add("arabic");
+            armTab.innerHTML = " ذراع الروبوت  <img class=\"robot-arm ms-1\" src=\"images/robot-arm.svg\">";
+            armTab.classList.add("arabic");
+            document.querySelector(".msg").innerHTML = "<h4 class=\"text-light text-end p-4\">،مرحبا<br> هل يمكنك مساعدتي على الحركة؟</h4>";
+            document.querySelector(".msg").classList.add("arabic");
+        } else {
+            movementTab.innerHTML = "Robot Movement <img class=\"directions\" src=\"images/controller.svg\">";
+            movementTab.classList.remove("arabic");
+            armTab.innerHTML = " ذراع الروبوت  <img class=\"robot-arm ms-1\" src=\"images/robot-arm.svg\">";
+            armTab.classList.remove("arabic");
+            document.querySelector(".msg").innerHTML = "<h3 class=\"text-light p-4\">Hello,<br> can you help me move?</h3>";
+            document.querySelector(".msg").classList.remove("arabic");
+
+        }
+    });
+});
